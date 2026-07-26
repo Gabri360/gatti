570bc94b3d5051dfbd3b52be8d35eddf0557042f
Dolphin & Turtle <iamreallybadatphysics@gmail.com>
2026-07-26 19:51:24 +0200

    image displacement and scaling

--- README.md
+++ README.md
@@ -2,9 +2,21 @@
 The program is currently useless. I am writing this because I have a bunch of images of
 notes etc...
 
+So... *sloppytop* is not a great name for anything at all. If someone has a better
+name please recommend.
+
 # Usage
 1. Go to the "settings.json" file and set the root variable according to your environment.
 2. Run the "main.py" file with a valid Python (version > 3.14.5) interpreter.
 3. Extend the root path with keyboard input.
 4. When the hints collapse to a single one press enter.
 5. Repeat until encountering a valid image file, pressing enter will load the image.
+6. You can move images by dragging them, you can scale images with by scrolling.
+
+# Upcoming
+1. Fixing scaling issues.
+2. Save program state.
+3. Improve the search box.
+
+# Issues
+1. The current scaling implementation is completely broken and dangerous.
--- main.py
+++ main.py
@@ -32,23 +32,51 @@ search_box = SearchBox(
 images_offline = []
 images_online = []
 pos = []
+width = []
+height = []
+scale = []
 
+x0, y0 = 0, 0
 while True:
-
+    dx, dy = 0, 0
+    dz = 0
     for event in pg.event.get():
         search_box.listen(event)
         if search_box.result is not None:
             srf = pg.image.load(search_box.result).convert_alpha()
             images_offline.append(srf)
-            images_online.append(pg.transform.smoothscale_by(srf, .5*WIDTH / srf.get_width()))
+            srf = pg.transform.smoothscale_by(srf, .5*WIDTH / srf.get_width())
+            images_online.append(srf)
+            pos.append(((WIDTH - srf.get_width())/2, (HEIGHT - srf.get_height())/2))
+            width.append(srf.get_width())
+            height.append(srf.get_height())
+            scale.append(1.0)
             search_box.result = None
+
+        if event.type == pg.MOUSEWHEEL:
+            dz = 1.0 - event.y *.1
+
+        if event.type == pg.MOUSEMOTION and event.buttons[0]:
+            x0, y0 = event.pos
+            dx, dy = event.rel
+
         if event.type == pg.KEYDOWN:
             if event.key == pg.K_ESCAPE:
                 pg.quit()
                 exit()
 
     screen.fill(COLOR_BACKGROUND)
-    for srf in images_online:
-        screen.blit(srf, (0, 0))
+
+    locked = False
+    for i, ((x, y), w, h, s, srf, srf_off) in enumerate(zip(pos, width, height, scale, images_online, images_offline)):
+        if x < x0 < x + w and y < y0 < y + h and not locked:
+            if dz != 0:
+                scale[i] /= dz
+                print(s)
+                images_online[i] = pg.transform.smoothscale_by(srf_off, s)
+            pos[i] = (x + dx, y + dy)
+            locked = True
+        screen.blit(srf, (x, y))
+
     search_box.draw(screen)
     pg.display.update()
