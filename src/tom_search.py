d9cfe0ce054c11dfe342b412c7214291d9b776cb
Dolphin & Turtle <iamreallybadatphysics@gmail.com>
2026-08-04 01:37:02 +0200

    general improvements on the search box

--- src/tom_search.py
+++ src/tom_search.py
@@ -12,15 +12,34 @@ class TomSearch:
     part: str
     walk: list[str]
     hint: list[str]
+    index: int
 
     @classmethod
     def empty(cls):
-        return cls(part="", walk=[], hint=[])
+        return cls(part="", walk=os.path.abspath("").split('/'), hint=[], index=0)
+
+    def genhint(self):
+
+        LEGAL = [".jpg", ".jpeg", ".png", ".webp"]
+
+        # absolute path is used because it hints at possible sub-directories
+        path = '/'.join(self.walk)
+
+        # only some specific names are worth exploring
+        self.hint = sorted(
+            name for name in os.listdir(path)
+
+            # filter names that are incompatible with the query
+            if self.part in name[:len(self.part)] and
+
+            # filter unrecognized formats (that aren't directories)
+            (any(fmt in name for fmt in LEGAL) or os.path.isdir(f"{path}/{name}"))
+
+        )
 
     def run(self, screen, font, bg, pos):
 
-        # generate hints
-        self.hint = sorted(os.listdir(os.path.abspath('/'.join(self.walk))))
+        self.genhint()
 
         while True:
             for event in pg.event.get():
@@ -31,22 +50,30 @@ class TomSearch:
 
                 # pop latest user-input character if the BACKSPACE key is pressed
                 if event.key == pg.K_BACKSPACE:
-                    self.part = self.part[:-1]
+                    if len(self.part) > 0:
+                        self.part = self.part[:-1]
+                    else:
+                        self.walk.pop()
+                        
+                # roll through hints
+                elif event.key == pg.K_TAB:
+                    self.index = (self.index + 1) % len(self.hint)
 
-                # process user-input if the RETURN key is pressed
+                # process user-input if the RETURN key is pressed and a possible match exists
                 elif event.key == pg.K_RETURN:
 
+                    # this conditional is separated from the previous so that RETURN wouldn't be read as unicode
+                    if len(self.hint) == 0:
+                        continue
+
                     # fit user-input to nearest hint and extend walk
-                    fit = [n for n in self.hint if self.part in n[:len(self.part)]][0]
-                    self.walk.append(fit)
+                    self.walk.append(self.hint[self.index])
                     self.part = ""
 
                     # transition to the BOARD if the walk can no longer be extended otherwise generate new hints
                     path = '/'.join(self.walk)
                     if not os.path.isdir(path):
                         return ts.TomState.BOARD
-                    else:
-                        self.hint = [n for n in sorted(os.listdir(path))]
 
                 # exit program if the ESC key is pressed
                 elif event.key == pg.K_ESCAPE:
@@ -55,6 +82,8 @@ class TomSearch:
                 # append user-input if the pressed key ALPHA-NUMERICAL
                 else:
                     self.part += event.unicode
+
+                self.genhint()
 
             # draw background
             screen.blit(bg, (0, 0))
@@ -67,7 +96,7 @@ class TomSearch:
             
             # draw search box (completion hints)
             fade = 255 / 2
-            for i, p in enumerate(n for n in self.hint if self.part in n):
+            for i, p in enumerate(self.hint[self.index:] + self.hint[:self.index]):
 
                 # fade out rendered text
                 text = '/'.join(self.walk + [p])
