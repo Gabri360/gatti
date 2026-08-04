15b6b4ef9c4cd2e6d0ce87a2b22bf41375808db4
Dolphin & Turtle <iamreallybadatphysics@gmail.com>
2026-08-04 17:35:24 +0200

    working towards windows installation

--- install.py
+++ install.py
@@ -6,51 +6,81 @@ from os import listdir as ls
 from subprocess import call
 
 
-if os_name == "nt":
-    print("The installation is currently unavailable for windows")
-    exit()
-
-elif os_name != "posix":
+if os_name not in {"posix", "nt"}:
     print("The current host isn't supported, can't install")
     exit()
 
 
-if len(argv) == 1:
+elif os_name == "nt":
 
-    # Install virtual environment
-    call(["mkdir", "/usr/local/lib/tom"])
-    call(["python", "-m", "venv", "/usr/local/lib/tom/venv"])
-    call(["/usr/local/lib/tom/venv/bin/python", "-m", "pip", "install", "pygame-ce", "--quiet", "--quiet"])
+    print("The windows host isn't currently supported, can't install")
+    exit()
+
+    if len(argv) == 1:
+        # Install virtual environment
+        call(["md", r"\Program Files\tom"])
+        call(["python", "-m", "venv", r"\Program Files\tom"])
+        call([r"\Program Files\tom\venv\Scripts\python", "-m", "pip", "install", "pygame-ce", "--quiet", "--quiet"])
+        
+        # Install assets
+        call(["xcopy", "themes", r"\Program Files\tom\themes"])
+        
+        # Install source
+        call(["xcopy", "src", r"\Program Files\tom\src"])
+        with open("/usr/local/src/tom/path", "w") as file:
+            file.write("/usr/local/etc/tom")
+        
+        # Install config
+        with open(r"\Program Files\tom\settings.json", "w") as file:
+            json.dump({
+                "width": 1280,
+                "height": 720,
+                "theme": r"\Program Files\tom\themes\default.json"
+            }, file, indent=4)
+        
+        # Install 'executable'
+        with open("/usr/local/bin/tom", "w") as file:
+            file.write("@echo off\n")
+            file.write(r"cmd /k \"cd /d C:\Program Files\tom\venv\Scripts & .\activate & cd /d C:\Program Files\tom & python -BO src\main.py\"")
+            call(["powershell", r"\"$s=(New-Object -COM WScript.Shell).CreateShortcut('C:\Users\USERNAME\Desktop\tom.lnk');$s.TargetPath='C:\Program Files\tom\tom.bat';$s.Save()\""])
     
-    # Install assets
-    call(["mkdir", "/usr/local/share/tom"])
-    call(["cp", "-r", "themes", "/usr/local/share/tom/"])
+    elif len(argv) == 2 and argv[1] == "uninstall":
+        call(["rd", "/s", "/q", r"\Program Files\tom"])
+
+elif os_name == "posix":
+    if len(argv) == 1:
     
-    # Install source
-    call(["mkdir", "/usr/local/src/tom"])
-    for fname in ls("src"):
-        call(["cp", f"src/{fname}", "/usr/local/src/tom/"])
-    with open("/usr/local/src/tom/path", "w") as file:
-        call(["echo", "-n", "/usr/local/etc/tom"], stdout=file)
-
-    # Install config
-    call(["mkdir", "/usr/local/etc/tom/"])
-    with open("/usr/local/etc/tom/settings.json", "w") as file:
-        json.dump({
-            "width": 1280,
-            "height": 720,
-            "theme": "/usr/local/share/tom/themes/default.json"
-        }, file, indent=4)
-
-    # Install 'executable'
-    with open("/usr/local/bin/tom", "w") as file:
-        call(["echo", "-e", "/usr/local/lib/tom/venv/bin/python -BO /usr/local/src/tom/main.py $@"], stdout=file)
-    call(["chmod", "+x", "/usr/local/bin/tom"])
-
-
-elif len(argv) == 2 and argv[1] == "uninstall":
-    call(["rm", "-rf", "/usr/local/lib/tom/"])
-    call(["rm", "-rf", "/usr/local/share/tom/"])
-    call(["rm", "-rf", "/usr/local/src/tom/"])
-    call(["rm", "-rf", "/usr/local/etc/tom/"])
-    call(["rm", "-f", "/usr/local/bin/tom"])
+        # Install virtual environment
+        call(["mkdir", "/usr/local/lib/tom"])
+        call(["python", "-m", "venv", "/usr/local/lib/tom/venv"])
+        call(["/usr/local/lib/tom/venv/bin/python", "-m", "pip", "install", "pygame-ce", "--quiet", "--quiet"])
+        
+        # Install assets
+        call(["mkdir", "/usr/local/share/tom"])
+        call(["cp", "-r", "themes", "/usr/local/share/tom/"])
+        
+        # Install source
+        call(["cp", "-r", "src", "/usr/local/src/tom"])
+        with open("/usr/local/src/tom/path", "w") as file:
+            file.write("/usr/local/etc/tom")
+    
+        # Install config
+        call(["mkdir", "/usr/local/etc/tom/"])
+        with open("/usr/local/etc/tom/settings.json", "w") as file:
+            json.dump({
+                "width": 1280,
+                "height": 720,
+                "theme": "/usr/local/share/tom/themes/default.json"
+            }, file, indent=4)
+    
+        # Install 'executable'
+        with open("/usr/local/bin/tom", "w") as file:
+            file.write("/usr/local/lib/tom/venv/bin/python -BO /usr/local/src/tom/main.py $@\n")
+        call(["chmod", "+x", "/usr/local/bin/tom"])
+    
+    elif len(argv) == 2 and argv[1] == "uninstall":
+        call(["rm", "-rf", "/usr/local/lib/tom/"])
+        call(["rm", "-rf", "/usr/local/share/tom/"])
+        call(["rm", "-rf", "/usr/local/src/tom/"])
+        call(["rm", "-rf", "/usr/local/etc/tom/"])
+        call(["rm", "-f", "/usr/local/bin/tom"])
