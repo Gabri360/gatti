3778700b35c8ea00e73a4dc55b73628811af072a
gabriele baldi <86059588+Gabri360@users.noreply.github.com>
2026-08-09 23:02:56 +0200

    Update src/gatti_board.py
    Co-authored-by: green <120983645+dolphinandturtle@users.noreply.github.com>

--- src/gatti_board.py
+++ src/gatti_board.py
@@ -202,7 +202,7 @@ class GattiBoard:
             # draw grid
 
             start_col = self.cam_pos.x - (self.cam_pos.x % tp.GRID_SPACING)
-            start_row = self.cam_pos.y - (self.cam_pos.y % tp.GRID_SPACING)
+            start_row = self.cam_pos.y - (self.cam_pos.y % gp.GRID_SPACING)
 
             n_col = int(gp.WIDTH / gp.GRID_SPACING / self.cam_scale)
             n_row = int(gp.HEIGHT / gp.GRID_SPACING / self.cam_scale)
